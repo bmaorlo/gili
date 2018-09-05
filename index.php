dd87d109439c4e59d8a2ab111d0a6c47eb9290c5
bmaorlo <opaoopa@gmail.com>
2018-09-05 09:57:43 +0300

    Change SMTP to gmail

--- index.php
+++ index.php
@@ -41,17 +41,18 @@ $mail->CharSet = 'UTF-8';
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
-$mail->SMTPDebug = 0;
-//Set the hostname of the mail server
-$mail->Host = 'mail.smtp2go.com';
+$mail->Host = 'smtp.gmail.com';
 //Set the SMTP port number - likely to be 25, 465 or 587
-$mail->Port = 25;
+$mail->Port = 587;
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication
 $mail->Username = 'mor.balo81@gmail.com';
 //Password to use for SMTP authentication
-$mail->Password = 'pAyZV0cWuPhJ';
+$mail->Password = 'guhparxdqvllnxrs';
+$mail->SMTPSecure = 'tls';
+
+$mail->Debugoutput = 'html';
 //Set who the message is to be sent from
 $mail->setFrom('gilgilmos@gmail.com', 'Gili Parties');
 //Set an alternative reply-to address
